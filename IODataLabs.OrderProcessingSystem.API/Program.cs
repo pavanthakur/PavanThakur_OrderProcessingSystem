fc2bc25d8b52723f80ffb52084eb2a26b35bc223
Pavan Thakur <pavan.thakur@gmail.com>
2024-12-28 07:57:06 +0530

    Added Swagger library support

--- IODataLabs.OrderProcessingSystem.API/Program.cs
+++ IODataLabs.OrderProcessingSystem.API/Program.cs
@@ -1,5 +1,6 @@
 using IODataLabs.OrderProcessingSystem.Application;
 using IODataLabs.OrderProcessingSystem.Infrastructure.DataContext;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,20 @@ builder.InjectApplicationDependencies();
 //builder.AddWebServices();
 
 builder.Services.AddControllers();
+
+// Register Swagger services
+builder.Services.AddEndpointsApiExplorer(); // Required for generating Swagger API documentation
+builder.Services.AddSwaggerGen(options =>
+{
+    // Optionally, you can customize the Swagger UI or API info
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1",
+        Description = "A sample API for Swagger demo",
+    });
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -24,6 +39,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger(); // Enable Swagger
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+        options.RoutePrefix = string.Empty; // Optional: Set Swagger UI at the root
+    });
     app.MapOpenApi();
 }
 
